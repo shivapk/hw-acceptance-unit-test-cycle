8af026093d2196188582c9d248dd1ebfdbc7b755
EC2 Default User <ec2-user@ip-172-31-44-115.us-east-2.compute.internal>
2019-10-22 02:50:50 +0000

    spec helper updated

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -13,14 +13,25 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
+    when /^the home\s?page$/
+      '/movies'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
-
+    
+    when /^the edit page for "(.*)"$/i
+      edit_movie_path(Movie.find_by_title($1))
+    
+    when /^the details page for "(.*)"$/i
+      movie_path(Movie.find_by_title($1))
+      
+    when /^the similar movies page for "(.*)"$/i
+      @movie = Movie.find_by_title($1)
+      same_director_movies_path(@movie)
+      
     else
       begin
         page_name =~ /^the (.*) page$/
