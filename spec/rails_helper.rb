560a671a30184421e9a7c343a077b9031b410bf6
EC2 Default User <ec2-user@ip-172-31-44-115.us-east-2.compute.internal>
2019-10-22 05:44:36 +0000

    paths changer

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,6 @@
 require 'simplecov'
 SimpleCov.start 'rails'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
@@ -52,9 +51,4 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
-
-  # Filter lines from Rails gems in backtraces.
-  config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
 end
