8287150c5189386a571107d1f3659e7910cd8fe3
EC2 Default User <ec2-user@ip-172-31-44-115.us-east-2.compute.internal>
2019-10-22 02:16:21 +0000

    director field to movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :director)
   end
 
   def show
@@ -59,6 +59,15 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
+  end
+  
+  def same_director
+    @movie = Movie.find(params[:id])
+    if @movie.director.nil? || @movie.director.empty?
+      flash[:notice] = "'#{@movie.title}' has no director info"
+      redirect_to movies_path 
+    end
+    @movies = Movie.same_director(@movie.director)
   end
 
 end
